8a312a548cac610853d554d1f96e1320fecb49fe
Михаил <mmvlasko@mai.education>
2024-02-19 19:08:48 +0300

    21

--- 21/21.sh
+++ 21/21.sh
@@ -1,5 +1,34 @@
-f=$(cat "wef")
-for (( i = 0; i < 5; i++ )); do
-    echo "${f:i:1}"
+if [[ $1 = "?" ]]; then
+  echo "Usage:"
+  echo "  $(basename -- "$0") <maximum file size (bytes)> <suffix> <output file name> [-i] [-d]"
+  echo $'\nKeys:\n  -i    Show information about the progress of the merger'
+  echo $'  -d    Delete source files'
+  read -r
+  exit
+fi
+
+home=$(pwd)
+touch "$3"
+
+for name in "$home"/*
+do
+  if [[ -f $name ]]; then
+    extension="${name##*.}"
+    size=$(stat -c %s "$name")
+    if [[ $size -le $1 && $extension = "$2" ]]; then
+      cat "$name" >> "$3"
+      if [[ $4 = "-i" || $5 = "-i" ]]; then
+        echo "INFO: file <$(basename -- "$name")> insert to <$3>"
+      fi
+      if [[ $4 = "-d" || $5 = "-d" ]]; then
+        rm "$name"
+      fi
+    fi
+  fi
 done
+
+if [[ $4 = "-i" || $5 = "-i" ]]; then
+  echo "INFO: completed"
+fi
+
 read -r
